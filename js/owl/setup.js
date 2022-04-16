ea48e4689098369bb5370f39e788e452202c9624
jonatas-melo-silva <jonatas891024@gmail.com>
2022-04-16 20:11:32 -0300

    Adiciona scripts

---
+++ js/owl/setup.js
@@ -0,0 +1,19 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    responsive:{
+        0:{
+            items:3
+        },
+        800:{
+            items:4
+        },
+        1100:{
+            items:5
+        },
+        1400:{
+            items:6
+        }
+    }
+})
